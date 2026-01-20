4eb2fa736badce4dfe30fb5b63d04b15979b0e96
tageorgia011981-rgb <tageorgia011981@gmail.com>
2026-01-20 09:14:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.onload = function() {
     const roomsBtn = document.getElementById('rooms-btn');
     const cabinet = document.getElementById('main-cabinet');
-    const metalPlate = roomsBtn.querySelector('.tab-card-inner');
+    const plate = roomsBtn.querySelector('.tab-card-inner');
 
-    roomsBtn.addEventListener('click', () => {
+    roomsBtn.onclick = function() {
+        // Slide cabinet
         cabinet.classList.toggle('show-me');
-        metalPlate.classList.toggle('active');
-        console.log("Cabinet and Glow toggled!");
-    });
-});
+        // Light up tab
+        plate.classList.toggle('active');
+    };
+};
