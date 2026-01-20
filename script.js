e10ca04d56c9d75b24876b17f134634a010c627b
tageorgia011981-rgb <tageorgia011981@gmail.com>
2026-01-20 09:00:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const roomsBtn = document.getElementById('rooms-btn');
 const cabinet = document.getElementById('main-cabinet');
 
+// This targets the specific metal plate inside the Rooms tab
+const roomsInner = roomsBtn.querySelector('.tab-card-inner');
+
 roomsBtn.onclick = function() {
+    // This makes the cabinet glide
     cabinet.classList.toggle('show-me');
-    console.log("Rooms button was clicked!");
+    
+    // This turns your CSS "Warm Glow" on and off
+    roomsInner.classList.toggle('active');
 };
